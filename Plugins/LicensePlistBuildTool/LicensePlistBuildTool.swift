b546461d31374e1f41b820fb5f05fcd4ff2e026d
Vladimir Vlasov <vladimir.vlasov@elements.nl>
2023-02-12 14:34:13 +0100

    Rework plugin source

--- Plugins/LicensePlistBuildTool/LicensePlistBuildTool.swift
+++ Plugins/LicensePlistBuildTool/LicensePlistBuildTool.swift
@@ -11,20 +11,7 @@ import PackagePlugin
 enum LicensePlistBuildToolError: Error {
     case workspaceNotFound
     case packageResolvedFileNotFound
-}
-
-@main
-struct LicensePlistBuildTool: BuildToolPlugin {
-    func createBuildCommands(context: PluginContext,
-                             target: Target) async throws -> [Command] {
-        let tool = try context.tool(named: "LicensePlist")
-        return [
-            .prebuildCommand(displayName: "LicensePlist is processing licenses...",
-                             executable: tool.path,
-                             arguments: [],
-                             outputFilesDirectory: context.pluginWorkDirectory)
-        ]
-    }
+    case configFileNotFound
 }
 
 #if canImport(XcodeProjectPlugin)
@@ -32,92 +19,48 @@ import XcodeProjectPlugin
 
 extension LicensePlistBuildTool: XcodeBuildToolPlugin {
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
-        let tool = try context.tool(named: "LicensePlist")
-        
+        // The folder with checked out package sources
         let checkoutDirectoryPath = context.pluginWorkDirectory
             .removingLastComponent()
             .removingLastComponent()
             .removingLastComponent()
             .removingLastComponent()
             .appending(subpath: "checkouts")
-//        print("🐶 \(try FileManager.default.contentsOfDirectory(atPath: checkoutDirectoryPath.string))")
         
-//        context.xcodeProject.directory
-        
+        // Gets the workspace path in the project folder
         let fileManager = FileManager.default
         let projectDirectoryItems = try fileManager.contentsOfDirectory(atPath: context.xcodeProject.directory.string)
         guard let workspacePath = projectDirectoryItems.first(where: { $0.hasSuffix(".xcworkspace") }) else {
             throw LicensePlistBuildToolError.workspaceNotFound
         }
         
-        let resolvedPath = Path(fileManager.currentDirectoryPath)
+        // Package.resolved file path inside the workspace
+        let packageResolvedPath = Path(fileManager.currentDirectoryPath)
             .appending(subpath: workspacePath)
             .appending(subpath: "xcshareddata/swiftpm/Package.resolved")
-        guard fileManager.fileExists(atPath: resolvedPath.string) else {
+        guard fileManager.fileExists(atPath: packageResolvedPath.string) else {
             throw LicensePlistBuildToolError.packageResolvedFileNotFound
         }
         
-//        let xcodeprojPackageResolvedPath = validatedPath
-//            .appendingPathComponent("project.xcworkspace")
-//            .appendingPathComponent("xcshareddata")
-//            .appendingPathComponent("swiftpm")
-//            .appendingPathComponent("Package.resolved")
-//
-//        let xcworkspacePackageResolvedPath = validatedPath
-//            .deletingPathExtension()
-//            .appendingPathExtension("xcworkspace")
-//            .appendingPathComponent("xcshareddata")
-//            .appendingPathComponent("swiftpm")
-//            .appendingPathComponent("Package.resolved")
-        
-//        let projDir = context.xcodeProject.directory
+        // Checks LicensePlist config
         let configPath = context.xcodeProject.directory.appending(subpath: "license_plist.yml")
-//        let data = try Data(contentsOf: URL(fileURLWithPath: configPath.string))
-//        print("🐶 \(String(data: data, encoding: .utf8) ?? "")")
+        guard fileManager.fileExists(atPath: packageResolvedPath.string) else {
+            throw LicensePlistBuildToolError.configFileNotFound
+        }
         
-        // TODO: Get output path from the config
-        let outputDirectoryPath = context.pluginWorkDirectory.appending(subpath: "Acknowledgements")
-        
+        // Output directory inside build output directory
+        let outputDirectoryPath = context.pluginWorkDirectory.appending(subpath: "LicensePlist")
         try fileManager.createDirectory(atPath: outputDirectoryPath.string, withIntermediateDirectories: true)
         
-//        try fileManager.contentsOfDirectory(atPath: outputDirectoryPath.string).forEach { item in
-//            let path = outputDirectoryPath.appending(subpath: item)
-//            try fileManager.removeItem(atPath: path.string)
-//        }
-        
-//        let originalOutputPath = context.xcodeProject.directory.appending(subpath: "Resources/Settings.bundle")
-//        let originLatestResultPath = originalOutputPath.appending(subpath: "Acknowledgements.latest_result.txt")
-//        let latestResultPath = outputDirectoryPath.appending(subpath: "Acknowledgements.latest_result.txt")
-//        if fileManager.fileExists(atPath: originLatestResultPath.string) {
-//            try fileManager.copyItem(atPath: originLatestResultPath.string, toPath: latestResultPath.string)
-//        }
-        
-        // TODO: add warnings for '--build-tool' usage (like no checkout path, etc.)
-        // TODO: filter target dependencies
-        
-        // TODO: Use options
-        let plistPath = outputDirectoryPath.appending(subpath: "Acknowledgements.plist")
-        // TODO: Use prefix: "\(options.prefix).latest_result.txt"
-        let latestResultPath = outputDirectoryPath.appending(subpath: "Acknowledgements.latest_result.txt")
-        
         return [
-//            .buildCommand(displayName: "LicensePlist is processing licenses...",
-//                          executable: tool.path,
-//                          arguments: ["--build-tool",
-////                                      "--package-path", resolvedPath,
-//                                      "--package-checkout-path", checkoutDirectoryPath.string,
-//                                      "--output-path", outputDirectoryPath
-//                                     ],
-//                          outputFiles: [outputDirectoryPath])
-                        .prebuildCommand(displayName: "LicensePlist is processing licenses...",
-                                         executable: Path("/Users/vlad/misc/github/LicensePlist-Package 2023-02-12 12-50-51/Products/usr/local/bin/license-plist"), //tool.path,
-                                         arguments: ["--build-tool",
-                                                     "--config-path", configPath,
-                                                     "--package-path", resolvedPath,
-                                                     "--package-checkout-path", checkoutDirectoryPath.string,
-                                                     "--output-path", outputDirectoryPath
-                                                    ],
-                                         outputFilesDirectory: context.pluginWorkDirectory)
+            .prebuildCommand(displayName: "LicensePlist is processing licenses...",
+                             executable: try context.tool(named: "LicensePlist"),
+                             arguments: ["--build-tool",
+                                         "--config-path", configPath,
+                                         "--package-path", packageResolvedPath,
+                                         "--package-checkout-path", checkoutDirectoryPath.string,
+                                         "--output-path", outputDirectoryPath],
+                             outputFilesDirectory: context.pluginWorkDirectory)
         ]
     }
 }
